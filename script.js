bc60e07be45cb618ae338cb9e9e9f8856d9432a4
Nicholas Werner <ncwerner@gmail.com>
2020-10-23 11:48:28 -0700

    created functions and vars so questions load and time starts on btn click

--- script.js
+++ script.js
@@ -10,60 +10,96 @@ THEN the game is over
 WHEN the game is over
 THEN I can save my initials and score */
 
-/* Game Logic */ 
+/* Game Logic */
 
 //the start button is clicked and the time starts
-const startButton = document.getElementById("start-btn")
+const startButton = document.getElementById("start-btn");
+const choice1 = document.getElementById("choice1");
+const choice2 = document.getElementById("choice2");
+const choice3 = document.getElementById("choice3");
+const choice4 = document.getElementById("choice4");
+const cardTitle = document.querySelector(".card-title");
+const timer = document.getElementById("timer");
 
-startButton.addEventListener("click", startGame)
+let timerInterval;
 
-function startGame() {
-    currentQuestion = 0
-    timer()
-    loadNextQuestion(currentQuestion)
-    console.log("started")
-}
-  
-function timer() {
-    console.log("timer start")
-}
-let currentQuestion;
-const score = ""
+let currentQuestion = 0;
+let score = "";
 
 // question is prompted
 const questions = [
-    {
-        questionNumber1: "What is the answer",
-        a: 'house',
-        b: 'shoe',
-        c: 'apple',
-        d: 'horse',
-        answer: 'horse'    
-    },
-    {
-        questionNumber2: "What is the answer",
-        a: 'lace',
-        b: 'bananna',
-        c: 'car',
-        d: 'mouse',
-        answer: 'car'    
-    },
-    {
-        questionNumber3: "What is the answer",
-        a: 'fruit',
-        b: 'chicken',
-        c: 'cat',
-        d: 'dog',
-        answer: 'fruit'    
-    },
-]
-// if correct game continues
-function checkAnswer() {
+  {
+    questionNumber: "What is the answer",
+    a: "house",
+    b: "shoe",
+    c: "apple",
+    d: "horse",
+    answer: "horse",
+  },
+  {
+    questionNumber: "What is the answer",
+    a: "lace",
+    b: "bananna",
+    c: "car",
+    d: "mouse",
+    answer: "car",
+  },
+  {
+    questionNumber: "What is the answer",
+    a: "fruit",
+    b: "chicken",
+    c: "cat",
+    d: "dog",
+    answer: "fruit",
+  },
+];
 
+function startGame() {
+  startTime();
+  loadNextQuestion(currentQuestion);
+  console.log("started");
+}
+
+function startTime() {
+  console.log("timer start");
+  let secondsLeft = 60;
+
+  timerInterval = setInterval(function () {
+    secondsLeft--;
+    timer.textContent = secondsLeft + " seconds left";
+
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+      //   sendMessage();
+    }
+  }, 1000);
 }
 
 function loadNextQuestion(currentQuestion) {
-    console.log(questions[currentQuestion])
+  console.log(questions[currentQuestion]);
+  cardTitle.textContent = questions[currentQuestion].questionNumber;
+  choice1.textContent = questions[currentQuestion].a;
+  choice2.textContent = questions[currentQuestion].b;
+  choice3.textContent = questions[currentQuestion].c;
+  choice4.textContent = questions[currentQuestion].d;
+
+  // Add event listeners to the four choice buttons such that it tells the user whether they're right or wrong, and changes the colors of the buttons, also changes score
+  // checkAnswer();
+
+  // If they're on the last question, clear interval and show the initials form
+  //clearInterval(timerInterval);
+}
+
+// if correct game continues
+function checkAnswer() {
+    
+  // Then the user will click the next button, which will iterate to the next question
+  // goToNext();
+}
+
+function goToNext() {
+  currentQuestion++;
+  loadNextQuestion(currentQuestion);
 }
 /* UI Logic */
 
@@ -74,6 +110,6 @@ function loadNextQuestion(currentQuestion) {
 // when the timer reaches 0 or no more questions the game is over
 
 // user can leave name and score in high scores in local storage
-function saveData() {
+function saveData() {}
 
-}
+startButton.addEventListener("click", startGame);
