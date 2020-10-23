32ba89778d0631430642cde0dec5bb66193082c7
Nicholas Werner <ncwerner@gmail.com>
2020-10-23 10:02:11 -0700

    added start function

--- script.js
+++ script.js
@@ -10,16 +10,70 @@ THEN the game is over
 WHEN the game is over
 THEN I can save my initials and score */
 
-// the start button is clicked and the time starts
+/* Game Logic */ 
 
-// question is prompted 
+//the start button is clicked and the time starts
+const startButton = document.getElementById("start-btn")
+
+startButton.addEventListener("click", startGame)
+
+function startGame() {
+    currentQuestion = 0
+    timer()
+    loadNextQuestion(currentQuestion)
+    console.log("started")
+}
+  
+function timer() {
+    console.log("timer start")
+}
+let currentQuestion;
+const score = ""
+
+// question is prompted
+const questions = [
+    {
+        questionNumber1: "What is the answer",
+        a: 'house',
+        b: 'shoe',
+        c: 'apple',
+        d: 'horse',
+        answer: 'horse'    
+    },
+    {
+        questionNumber2: "What is the answer",
+        a: 'lace',
+        b: 'bananna',
+        c: 'car',
+        d: 'mouse',
+        answer: 'car'    
+    },
+    {
+        questionNumber3: "What is the answer",
+        a: 'fruit',
+        b: 'chicken',
+        c: 'cat',
+        d: 'dog',
+        answer: 'fruit'    
+    },
+]
+// if correct game continues
+function checkAnswer() {
+
+}
+
+function loadNextQuestion(currentQuestion) {
+    console.log(questions[currentQuestion])
+}
+/* UI Logic */
 
 // user selects an answer
 
-// if correct game continues
-
-// if incorrect time is subtracted from timer 
+// if incorrect time is subtracted from timer
 
 // when the timer reaches 0 or no more questions the game is over
 
-// user can leave name and score in high scores
+// user can leave name and score in high scores in local storage
+function saveData() {
+
+}
