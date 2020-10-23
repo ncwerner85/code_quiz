233ebf68a2c1e3e41e5c17d3354fb28eeb7afb9f
Nicholas Werner <ncwerner@gmail.com>
2020-10-22 22:20:24 -0700

    pseudocode for the JavaScript complete

---
+++ script.js
@@ -0,0 +1,25 @@
+/* GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score */
+
+// the start button is clicked and the time starts
+
+// question is prompted 
+
+// user selects an answer
+
+// if correct game continues
+
+// if incorrect time is subtracted from timer 
+
+// when the timer reaches 0 or no more questions the game is over
+
+// user can leave name and score in high scores
